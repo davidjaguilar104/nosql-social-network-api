f808ffc9d2e8f02f3f4da32294540c3e6a279cbb
davidjaguilar104 <davidjaguilar104@gmail.com>
2022-01-21 15:53:16 -0600

    User model set up

--- models/User.js
+++ models/User.js
@@ -1,2 +1,33 @@
 const { Schema, model } = require("mongoose");
 const dateFormat = require("../utils/dateFormat");
+
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    match: /.+\@.+\..+/,
+  },
+  thoughts: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Thought",
+    },
+  ],
+  friends: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+});
+
+UserSchema.virtual("friendCount").get(function() {
+    return this.friends.length;
+})
