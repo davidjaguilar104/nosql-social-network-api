2636b308028360d16fcce390f5ec865aa10e601e
davidjaguilar104 <davidjaguilar104@gmail.com>
2022-01-21 19:16:30 -0600

    MVP

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -4,14 +4,24 @@ const {
     getThoughtById,
     createThought,
     updateThought,
-    deleteThought
+    deleteThought,
+    addReaction,
+    deleteReaction
 } = require("../../controllers/thought-controller");
 
-// get all thoughts and post a thought
-router.route("/").get(getAllThoughts).post(createThought);
+// get all thoughts 
+router.route("/").get(getAllThoughts);
+
+// post a thought
+router.route("/:userId").post(createThought);
 
 // get a single thought by id and also update and delete a thought by id
 router.route("/:id").get(getThoughtById).put(updateThought).delete(deleteThought);
 
+// post a reaction to a thought
+router.route("/:thoughtId/reactions").post(addReaction)
+
+// delete reaction from a thought
+router.route("/:thoughtId/reactions/:reactionId").delete(deleteReaction);
 
 module.exports = router;
